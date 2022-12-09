cf567f386122cc6cd8efe955144ebd4b3fd7592b
xdeathofmojox <xdeathofmojox@gmail.com>
2022-12-09 15:59:39 -0500

    Initial characters

---
+++ punkin_patch/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import CharacterList, CharacterDetail, CharacterCreate, CharacterUpdate, CharacterDelete
+
+urlpatterns = [
+    path('characters/', CharacterList.as_view(), name='characters'),
+    path('character/<int:pk>/', CharacterDetail.as_view(), name='character'),
+    path('character-create/', CharacterCreate.as_view(), name='character-create'),
+    path('character-update/<int:pk>/', CharacterUpdate.as_view(), name='character-update'),
+    path('character-delete/<int:pk>/', CharacterDelete.as_view(), name='character-delete'),
+]
--- punkin_patch/views.py
+++ punkin_patch/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Character
+from django.urls import reverse_lazy
 
-# Create your views here.
+class CharacterList(LoginRequiredMixin, ListView):
+    model = Character
+    context_object_name = 'characters'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['characters'] = context['characters'].filter(user=self.request.user)
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['characters'] = context['characters'].filter(
+                name__contains=search_input)
+
+        context['search_input'] = search_input
+
+        return context
+
+
+class CharacterDetail(LoginRequiredMixin, DetailView):
+    model = Character
+    context_object_name = 'character'
+    template_name = 'punkin_patch/character.html'
+
+
+class CharacterCreate(LoginRequiredMixin, CreateView):
+    model = Character
+    fields = ['name']
+    success_url = reverse_lazy('characters')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CharacterCreate, self).form_valid(form)
+
+
+class CharacterUpdate(LoginRequiredMixin, UpdateView):
+    model = Character
+    fields = ['name']
+    success_url = reverse_lazy('characters')
+
+
+class CharacterDelete(LoginRequiredMixin, DeleteView):
+    model = Character
+    context_object_name = 'character'
+    success_url = reverse_lazy('characters')
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(user=owner)
