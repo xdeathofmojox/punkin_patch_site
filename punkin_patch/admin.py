3a5f1944deb7862858a3263514389cb1f6c7d978
xdeathofmojox <xdeathofmojox@gmail.com>
2022-12-12 17:45:59 -0500

    Add models for instances of a patch and a vote

--- punkin_patch/admin.py
+++ punkin_patch/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Character, PatchTemplate
+from .models import Character, PatchTemplate, PatchInstance, Vote
 
 # Register your models here.
 admin.site.register(Character)
 admin.site.register(PatchTemplate)
+admin.site.register(PatchInstance)
+admin.site.register(Vote)
--- punkin_patch/models.py
+++ punkin_patch/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 def user_directory_path(instance, filename):
   
@@ -21,3 +22,51 @@ class PatchTemplate(models.Model):
 
     def __str__(self):
         return self.name
+
+class PatchInstance(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    template = models.ForeignKey(PatchTemplate, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Vote(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['characterID1', 'characterID2', 'instance'], name='No duplicate votes in patch instance')
+        ]
+
+    def validate_different_characters(self, c1, c2):
+        if c1 == c2:
+            raise ValidationError("{} is the same as {}".format(c1, c2))
+
+    def validate_winner_exists(self, winner, c1, c2):
+        if not (winner == c1 or winner == c2):
+            raise ValidationError("Winner: {} is not {} or {}".format(winner, c1, c2))
+
+    #TODO: This is a candidate for refactor. Probably a more efficient method of doing this
+    def validate_characters_in_patch(self, characters, patch):
+        charactersInPatch = [x for x in patch.characters.all()]
+        for character in characters:
+            if not character in charactersInPatch:
+                raise ValidationError("Character: {} is not in Patch: {}".format(character, patch))
+
+    def clean(self):
+        self.validate_different_characters(self.characterID1, self.characterID2)
+        self.validate_winner_exists(self.winningCharacterID, self.characterID1, self.characterID2)
+
+        patch = PatchTemplate.objects.get(id=self.instance.template.id)
+        self.validate_characters_in_patch([self.characterID1, self.characterID2], patch)
+
+    characterID1 = models.ForeignKey(
+        Character, on_delete=models.CASCADE, related_name="character1"
+    )
+    characterID2 = models.ForeignKey(
+        Character, on_delete=models.CASCADE, related_name="character2"
+    )
+    winningCharacterID = models.ForeignKey(
+        Character, on_delete=models.CASCADE, related_name="winningCharacter"
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    instance = models.ForeignKey(PatchInstance, on_delete=models.CASCADE)
